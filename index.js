f033d0fd2386029d2b46833496ebb4ca02c6ccf6
mrcreatist <ahk.verma102@gmail.com>
2020-08-02 11:45:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const app = require('express')();
+const cors = require('cors');
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+const fs = require('fs');
+const file = 'task-data.json';
+const baseData = [{ "name": "office", "data": [] }, { "name": "work", "data": [] }, { "name": "personal", "data": [] }];
+
+app.use(cors());
+
+function writeDataToFile(dataStore) {
+    let data = JSON.stringify(dataStore);
+    fs.writeFileSync(file, data);
+}
+
+function readDataFromFile() {
+    if (!fs.existsSync(file)) {
+        writeDataToFile(baseData);
+    }
+    let rawdata = fs.readFileSync(file);
+    return JSON.parse(rawdata);
+}
+
+io.on('connection', (socket) => {
+    console.log('welcome', socket.id);
+    socket.emit('fireInTheHole', readDataFromFile());
+
+    socket.on('fireInTheHole', () => console.log('fireInTheHole event triggered'));
+    socket.on('makeFireInTheHole', (data) => {
+        writeDataToFile(data);
+        socket.broadcast.emit('fireInTheHole', readDataFromFile());
+    });
+    socket.on('disconnect', () => console.log('disconnected from server'));
+});
+
+server.listen(process.env.PORT || 3333, () => console.log(`listening on ${process.env.PORT || 3333}`));
---
+++ start-server.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+cd ~/project/tasks/tasks-server
+npm start
